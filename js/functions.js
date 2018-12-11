6d8d936752c8c1455e383e507b24190a22ae87e2
menglish316 <MPE316@aol.com>
2018-12-11 15:54:25 -0500

    Mark's commit 12-11-18

--- js/functions.js
+++ js/functions.js
@@ -1,6 +1,15 @@
 $(document).ready(function(){
-
-
+    var secondaryMenuLinks = $(".secondary-menu-item");
+  //enable all of the links in the navbar to scroll to the targeted section of the page
+  secondaryMenuLinks.map(function(){
+    $(this).on("click", function(){
+      //get the target of each link
+      var id = $(this).children().attr("href");
+      //scroll to the target
+      //animation taken and adapted from http://stackoverflow.com/questions/6677035/jquery-scroll-to-element
+      $("html, body").animate({scrollTop: $(id).offset().top}, 1500);
+    });
+  });
 
 
 
