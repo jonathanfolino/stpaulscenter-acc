1b5c3734345cf7699ea543443b493f0285339003
menglish316 <MPE316@aol.com>
2018-12-18 17:20:10 -0500

    Mark's commit 12-18-18

--- js/functions.js
+++ js/functions.js
@@ -7,17 +7,24 @@ $(document).ready(function(){
         items:1,
     });
 
-
+    //get all of the secondary links
     var secondaryMenuLinks = $(".secondary-menu-item");
-  //enable all of the links in the navbar to scroll to the targeted section of the page
-  secondaryMenuLinks.map(function(){
-    $(this).on("click", function(){
-      //get the target of each link
-      var id = $(this).children().attr("href");
-      //scroll to the target
-      //animation taken and adapted from http://stackoverflow.com/questions/6677035/jquery-scroll-to-element
-      $("html, body").animate({scrollTop: $(id).offset().top}, 1500);
+    //enable all of the links in the navbar to scroll to the targeted section of the page
+    secondaryMenuLinks.map(function(){
+        $(this).on("click", function(){
+            //get the target of each link
+            var id = $(this).children().attr("href");
+            //if the secondary link clicked is on another page
+            if ($(id).length == 0){
+                var newLink = $(this).parent().prev().children().attr("href");
+                $(this).children().attr("href", newLink + id);
+            }
+            //secondary link clicked is on the same page
+            else{
+                //scroll to the target
+                //animation taken and adapted from http://stackoverflow.com/questions/6677035/jquery-scroll-to-element
+                $("html, body").animate({scrollTop: $(id).offset().top}, 1500);
+            }
+        });
     });
-  });
-
 });
